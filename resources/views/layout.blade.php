e5aa59cc5166efca8dde89a566e62e9fb38c6802
Diego Fernandez <u2011115775@continental.edu.pe>
2016-11-17 13:30:35 -0500

    Font awasome Fixed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,18 +10,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <title>Project Monterrey @yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
+  <!-- Bootstrap 3.3.6 >
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome -->
+  <!-- Font Awesome >
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
+  <!-- Ionicons >
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
+  <!-- Theme style >
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect.
-  -->
+  >
   <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -44,6 +44,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
 
         <link rel="stylesheet" href="{{ asset('css/skins/skin-blue.min.css') }}">
+
+        <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
 
         <!-- Optional theme -->
         <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css') }}">
@@ -88,7 +90,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <a href="#">
                       <div class="pull-left">
                         <!-- User Image -->
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="/images/avatar04.png" class="img-circle" alt="User Image">
                       </div>
                       <!-- Message title and timestamp -->
                       <h4>
@@ -451,11 +453,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 <!-- REQUIRED JS SCRIPTS -->
 
-<!-- jQuery 2.2.3 -->
+<!-- jQuery 2.2.3 >
 <script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
+<!-- Bootstrap 3.3.6 >
 <script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- AdminLTE App -->
+<!-- AdminLTE App >
 <script src="dist/js/app.min.js"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
